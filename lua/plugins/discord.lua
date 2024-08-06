e9f571846cc8432bfbb3ce78ad6982c7f5d74033
Robinxo <omrawat.work@gmail.com>
2024-08-06 19:37:29 +0530

    bruh

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,4 +1,4 @@
 return {
-  "IogaMaster/neocord",
-  event = "VeryLazy",
+  "andweeb/presence.nvim",
+  buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
 }
